f5ca2bd7d77b08cdc3c8bce9cddd70012c276c6b
ayaka.kitabora <ayaka.kitabora@theport.jp>
2020-06-11 17:32:28 +0900

    firebase設定したかった

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from './Firebase'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+class App extends Component {
+  state = {
+    user: null
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user: any) => {
+      this.setState({ user })
+    })
+  }
+  login() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+  }
+  logout() {
+    firebase.auth().signOut()
+  }
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    );
+  }
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ Firebase.ts
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+// Optionally import the services that you want to use
+import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
