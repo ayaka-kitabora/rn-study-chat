5721fa9fd5fcac0abd6f89845032ed874d394d6e
ayaka.kitabora <ayaka.kitabora@theport.jp>
2020-06-25 18:30:03 +0900

    wip ログイン

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import firebase from './Firebase';
 
 
 class App extends Component {
   state = {
-    user: null
+    user: null as any,
+    email: '' as string,
+    password: '' as string,
   }
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user: any) => {
       this.setState({ user })
     })
   }
+  signup () {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then((user: any) => {
+        if (user) {
+            console.log("Success to Signup")
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    })
+  }
   login() {
-    const provider = new firebase.auth.GoogleAuthProvider()
-    firebase.auth().signInWithRedirect(provider)
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then(response => {
+        alert("Login Success!");
+    })
+    .catch(error => {
+        alert(error.message);
+    });
   }
   logout() {
     firebase.auth().signOut()
@@ -22,7 +40,22 @@ class App extends Component {
   render () {
     return (
       <View style={styles.container}>
+        <View style={styles.row}>
+          <Text>メールアドレス: </Text>
+          <TextInput
+            style={styles.input}
+            value={this.state.email}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text>パスワード: </Text>
+          <TextInput
+            style={styles.input}
+            value={this.state.password}
+          />
+        </View>
         <TouchableOpacity
+          style={styles.submit}
           onPress={() => this.login()}
           >
             <Text>ログイン</Text>
@@ -40,4 +73,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row: {
+    width: 200,
+    flexDirection: 'row',
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  input: {
+    width: 150,
+    borderColor: '#333',
+    borderWidth: 1,
+    padding: 5
+  },
+  submit: {
+    width: 100,
+    borderColor: '#333',
+    borderWidth: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 5,
+    marginTop: 10,
+  }
 });
