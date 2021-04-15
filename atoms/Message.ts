f23849fc5562dd06349dd9ed8021bace65f47adb
ayaka.kitabora <ayaka.kitabora@theport.jp>
2021-04-15 17:28:23 +0900

    message atom追加

---
+++ atoms/Message.ts
@@ -0,0 +1,24 @@
+import { atom } from 'recoil';
+
+export type Message = {
+  user: {
+    name: string
+    user_url: string
+  }
+  created_at: {
+    seconds: number
+    nanoseconds: number
+  }
+  id: string
+  text: string
+}
+
+export const messageState = atom({
+  key: 'messageState',
+  default: {} as Message
+})
+
+export const messageListState = atom({
+  key: 'messageListState',
+  default: [] as Message[]
+})
